68d062317eba49732b2c7afd3cdbf49309ec09a6
yxjshenzhen <718586216@qq.com>
2014-09-04 22:25:13 +0800

    浏览器访问http://localhost:port/AppDemo/index.php入口文件 自动生成ThinkPHP项目框架目录～

---
+++ AppDemo/index.php
@@ -0,0 +1,10 @@
+<?php
+	//定义项目名称
+    define('APP_NAME','App');
+    //定义项目路径
+    define('APP_PATH','./App/');
+    // define('APP_DEBUG',TRUE);
+	define('APP_DEBUG',true);
+	
+	include './ThinkPHP/ThinkPHP.php';
+?>
